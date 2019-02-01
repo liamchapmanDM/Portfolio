9a39bcc58b636f0931273cf950468a594ebe661a
Liam Chapman <liam.chapman@myport.ac.uk>
2019-02-01 12:00:50 +0000

    Nav and Footer Updates

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
 // Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
+window.onscroll = function() {NewNav()};
+function NewNav() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace("w3-card w3-animate-top w3-white", "");
     }
 }
 
